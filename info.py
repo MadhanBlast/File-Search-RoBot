67e9e61b8256f5760815f76e6d9f6d5af1a0d713
MadhanBlastz <159687811+MadhanBlastz@users.noreply.github.com>
2024-06-26 23:19:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ PICS = (environ.get('PICS', 'https://wallpapercave.com/wp/wp7805698.jpg')).split
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2021408974').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001894642188').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002092756498').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '2021408974').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_channel = environ.get('AUTH_CHANNEL','-1001646636453')
@@ -36,7 +36,7 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Manoharasai:Manoharasai@cluster0.9cmxi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "manoharfiles")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1001112983591'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002092756498'))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'TBAdminBot')
 
 # Others
